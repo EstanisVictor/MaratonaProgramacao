e7937fe5d7efa03f6348e381016826ab8d724a0c
EstanisVictor <vitorestanislau1@gmail.com>
2022-12-14 00:18:31 -0300

    FCFS

--- Training_12-03-22/D.cpp
+++ Training_12-03-22/D.cpp
@@ -2,26 +2,68 @@
 #include <vector>
 using namespace std;
 
+int inserctionSort(double vet[], int i, int size)
+{
+    double value = vet[i];
+    int j = i;
+
+    while (j > 0 && vet[j - 1] < value)
+    {
+        vet[j] = vet[j - 1];
+        j--;
+    }
+
+    vet[j] = value;
+
+    if (i + 1 <= size)
+    {
+        inserctionSort(vet, i + 1, size);
+    }
+}
+
 int main()
 {
     int ins, n;
     cin >> ins;
     cin >> n;
 
-    vector<string> nomes;
-    vector<double> tam;
+    string nomes[ins];
+    double tam[ins];
+    double copia[ins];
+
+    int size = sizeof(tam) / sizeof(double);
+
+    vector<string> time;
 
     for (size_t i = 0; i < ins; i++)
     {
-        string s;
-        getline(cin, s);
-
-        double d;
-        cin >> d;
-
-        nomes.push_back(s);
-        tam.push_back(d);
+        cin >> nomes[i];
+        cin >> tam[i];
+        copia[i] = tam[i];
     }
+
+    inserctionSort(tam, 1, size - 1);
+
+    for (size_t i = 0; i < ins; i++)
+    {
+        
+        for (size_t j = 0; j < ins; j++)
+        {
+            if(copia[i] == tam[j]){
+                
+            }
+        }
+        
+    }
+    
 
     return 0;
 }
+/*
+5 3
+Marcos 1.5
+Joao 1.6
+Marcelo 2.0
+Heitor 1.8
+Jose 1.7
+*/
--- processos.cpp
+++ processos.cpp
@@ -1,50 +1,91 @@
 #include <bits/stdc++.h>
+#include <unistd.h>
 using namespace std;
 
-class Process{
-    public:
-        int time;
-        bool interrupt;
-        int duration_interrupt;
-    Process(int _time, bool _interrupt, int _duration){
-        time = _time;
+queue<string> queue_process;
+
+class Process
+{
+public:
+    string nameProcess;
+    int timeProcess;
+    bool interrupt;
+    int duration_interrupt;
+    Process(int _time, bool _interrupt, int _duration, string _nameProcess)
+    {
+        nameProcess = _nameProcess;
+        timeProcess = _time;
         interrupt = _interrupt;
         duration_interrupt = _duration;
     }
 };
 
+void printCounter(int counter)
+{
+    for (size_t i = 0; i < counter; i++)
+    {
+        cout << "\r" << i + 1 << "s";
+        sleep(1);
+    }
+    cout << endl;
+}
+
+void FCFS(vector<Process *> processes)
+{
+    int momentInterrupt = 0;
+
+    for (size_t i = 0; i < processes.size(); i++)
+    {
+
+        if (processes[i]->interrupt)
+        {
+            // Houve a interrupção
+            momentInterrupt = (processes[i]->timeProcess) / 2;
+            processes[i]->interrupt = false;
+            processes[i]->timeProcess = (processes[i]->timeProcess) / 2;
+            processes.push_back(processes[i]);
+        }
+        else
+        {
+            // Não houve interrupção, vai executar o tempo total do processo
+            momentInterrupt = processes[i]->timeProcess;
+        }
+
+        cout << "-----------------------------------------------------" << endl;
+        cout << processes[i]->nameProcess << endl;
+        printCounter(momentInterrupt);
+    }
+}
+
 int main()
 {
-    vector<Process*> processos;
-
+    vector<Process *> processes;
 
     for (size_t i = 0; i < 3; i++)
     {
-        int duracao = 0;
+        int duration = 0;
 
-        cout << "Tempo do processo " << i+1 << endl; 
+        cout << "Tempo do processo " << i + 1 << "?" << endl;
         int addTime;
         cin >> addTime;
 
-        cout << "Interrupcao P" << i+1 << endl;
-        bool interup;
-        cin >> interup;
+        cout << "P" << i + 1 << " tem interrupcao?" << endl;
+        cout << "0 - NAO\n1 - SIM" << endl;
+        bool interuptQuestion;
+        cin >> interuptQuestion;
 
-        if(interup){
+        if (interuptQuestion)
+        {
             srand(time(NULL));
-            duracao = rand()%10;
+            duration = rand() % 10;
         }
 
-        Process *p = new Process(addTime, interup, duracao);
+        Process *p = new Process(addTime, interuptQuestion, duration, "P" + to_string(i + 1));
 
-        processos.push_back(p);
+        processes.push_back(p);
     }
-    
-    for (size_t i = 0; i < processos.size(); i++)
-    {
-        
-    }
-    
+
+    FCFS(processes);
 
     return 0;
 }
